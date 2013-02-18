989c45f8243ed68a47a2f67c8ffa89040a569ae6
josiahpeters <josiahpeters@gmail.com>
2013-02-18 11:26:02 -0700

    Refactored existing project into new project. Added default form application code to AppMain.cs

---
+++ QwikShot.WinApp/AppMain.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QwikShot.WinApp
+{
+    public class AppMain : Form
+    {
+        [STAThread]
+        static void Main()
+        {
+            GlobalKeyHook.SetHook();
+            Application.Run(new AppMain());
+        }
+
+        private NotifyIcon systemTrayIcon;
+        private System.ComponentModel.IContainer components;
+        private ContextMenu systemTrayMenu;
+
+        private ScreenShotPictureBox screenshotOverlay;
+
+        private Rectangle screenBounds = Rectangle.Empty;
+        private Point leftMostScreenCoordinate = new Point(0,0);
+
+        public AppMain()
+        {
+            // set up global screenshot shortcut callback
+            GlobalKeyHook.KeyPress += GlobalKeyHook_KeyPress;
+
+            // figure out the size of all potential monitors combined
+            CalculateScreenSize();
+
+            InitializeComponent();
+        }
+        private void CalculateScreenSize()
+        {
+
+            foreach (Screen s in Screen.AllScreens)
+            {
+                screenBounds = Rectangle.Union(screenBounds, s.Bounds);
+
+                if (s.WorkingArea.X < leftMostScreenCoordinate.X)
+                {
+                    leftMostScreenCoordinate.X = s.WorkingArea.X;
+                    leftMostScreenCoordinate.Y = s.WorkingArea.Y;
+                }
+            }
+        }
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();            
+
+            this.SuspendLayout();
+
+            this.systemTrayMenu = new ContextMenu();
+            this.systemTrayIcon = new NotifyIcon(this.components);
+
+            // set up system tray icon
+            this.systemTrayIcon.ContextMenu = this.systemTrayMenu;
+            this.systemTrayIcon.Text = "QwikShot";
+            this.systemTrayIcon.Visible = true;
+            this.systemTrayIcon.Icon = new Icon(global::QwikShot.WinApp.Properties.Resources.icon_application, 32, 32);
+
+            // set up form styles
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Normal;
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Icon = new Icon(global::QwikShot.WinApp.Properties.Resources.icon_application, 32, 32);
+
+            // set up the overlay for holding and resizing the screenshot
+            screenshotOverlay = new ScreenShotPictureBox();
+
+            // add overlay to the form
+            this.Controls.Add(screenshotOverlay);
+
+            this.ResumeLayout(false);
+        }
+
+        void GlobalKeyHook_KeyPress(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ QwikShot.WinApp/GlobalKeyHook.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QwikShot.WinApp
+{
+    public class GlobalKeyHook
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        public delegate void ChangedEventHandler(object sender, EventArgs e);
+
+        public static event ChangedEventHandler KeyPress;
+
+        public static void SetHook()
+        {
+            _hookID = SetHook(_proc);
+        }
+
+        public static void Unhook()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(
+            int nCode, Int32 wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, Int32 wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                // keydown (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)
+                // just worry about keydown for now.
+
+                // keypress up (wParam == WM_KEYDOWN)
+                if (wParam == WM_KEYDOWN)
+                {
+                    int vkCode = Marshal.ReadInt32(lParam);
+                    if (Keys.F12 == (Keys)vkCode && Keys.Control == Control.ModifierKeys)
+                    {
+                        if (KeyPress != null)
+                            KeyPress(null, null);
+                    }
+                }
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, Int32 wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
